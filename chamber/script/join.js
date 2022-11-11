90e1f82273c8ae9b7bcb31c47c3bf751be769fe7
NReeve160 <reeve.nathan160@gmail.com>
2022-11-11 01:53:58 -0500

    join page

---
+++ chamber/script/join.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('#form');
+const input = document.querySelector('#bposition');
+const output = document.querySelector('#output');
+
+const re = /[a-z]/;
+
+function testInfo(bpositionInput) {
+  const ok = re.exec(bpositionInput.value);
+
+  output.textContent = ok
+    ? `Thanks, your phone number is ${ok[0]}`
+    : `${bpositionInput.value} Please only use alpha characters, hypens, and spaces
+    minimum of seven characters!`;
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  testInfo(input);
+});
