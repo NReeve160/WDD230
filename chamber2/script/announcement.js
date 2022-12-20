9eb089a0372b622905db6e22f3487077b0178f8c
NReeve160 <reeve.nathan160@gmail.com>
2022-12-20 00:43:47 -0500

    adding announcement

---
+++ chamber2/script/announcement.js
@@ -0,0 +1,9 @@
+
+const day = new Date().getDay();
+
+if (day == 1) {
+    document.querySelector(".announcement").style.display = "block";
+}
+else if (day == 2) {
+    document.querySelector(".announcement").style.display = "block";
+}
